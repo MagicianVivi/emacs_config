8b2cef6f0ffdf49a848e023da1bf25ece1cca2d1
Vivien Alger <vivien@nomalab.com>
2020-10-03 18:12:45 +0200

    Remove idris mode

--- init.el
+++ init.el
@@ -13,7 +13,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(async-shell-command-buffer (quote new-buffer))
+ '(async-shell-command-buffer 'new-buffer)
  '(auth-source-cache-expiry nil)
  '(auth-source-do-cache nil)
  '(auth-sources nil)
@@ -22,54 +22,42 @@
  '(compilation-scroll-output t)
  '(cursor-color nil)
  '(custom-safe-themes
-   (quote
-    ("ea4ae3c179b4fd23f50b50062ca0312f7bf0151aa4ba8c10088bba87943d8b93" "98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" "16fd69242d5383a431bc49ed3b567dbce148a4991242baa11ee6367ca93705e2" "f67652440b66223b66a4d3e9c0ddeddbf4a6560182fa38693bdc4d940ce43a2e" "0f0022c8091326c9894b707df2ae58dd51527b0cf7abcb0a310fb1e7bda78cd2" "5310b88333fc64c0cb34a27f42fa55ce371438a55f02ac7a4b93519d148bd03d" "8d737627879eff1bbc7e3ef1e9adc657207d9bf74f9abb6e0e53a6541c5f2e88" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "31a01668c84d03862a970c471edbd377b2430868eccf5e8a9aec6831f1a0908d" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "1297a022df4228b81bc0436230f211bad168a117282c20ddcba2db8c6a200743" "3a727bdc09a7a141e58925258b6e873c65ccf393b2240c51553098ca93957723" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" "57072d797dc09fcf563051a85a29d6a51d6f2b1a602e029c35b05c30df319b2a" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
+   '("d88049c628f3a8a92f9e46982d3e891867e4991de2b3a714f29f9f5eb91638c1" "08ef1356470a9d3bf363ffab0705d90f8a492796e9db489936de4bde6a4fdb19" "ea4ae3c179b4fd23f50b50062ca0312f7bf0151aa4ba8c10088bba87943d8b93" "98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" "16fd69242d5383a431bc49ed3b567dbce148a4991242baa11ee6367ca93705e2" "f67652440b66223b66a4d3e9c0ddeddbf4a6560182fa38693bdc4d940ce43a2e" "0f0022c8091326c9894b707df2ae58dd51527b0cf7abcb0a310fb1e7bda78cd2" "5310b88333fc64c0cb34a27f42fa55ce371438a55f02ac7a4b93519d148bd03d" "8d737627879eff1bbc7e3ef1e9adc657207d9bf74f9abb6e0e53a6541c5f2e88" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "31a01668c84d03862a970c471edbd377b2430868eccf5e8a9aec6831f1a0908d" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "1297a022df4228b81bc0436230f211bad168a117282c20ddcba2db8c6a200743" "3a727bdc09a7a141e58925258b6e873c65ccf393b2240c51553098ca93957723" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" "57072d797dc09fcf563051a85a29d6a51d6f2b1a602e029c35b05c30df319b2a" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default))
  '(dired-listing-switches "-alh")
  '(electric-indent-mode t)
  '(electric-pair-mode t)
- '(elm-indent-after-keywords
-   (quote
-    (("of" 2)
-     ("in" 2 0)
-     ("{" 2)
-     "if" "then" "else" "let")))
+ '(elm-indent-after-keywords '(("of" 2) ("in" 2 0) ("{" 2) "if" "then" "else" "let"))
  '(elm-indent-offset 2)
  '(flycheck-checkers
-   (quote
-    (elm idris c/c++-clang c/c++-cppcheck coffee coffee-coffeelint css-csslint d-dmd elixir emacs-lisp emacs-lisp-checkdoc erlang go-gofmt go-build go-test haml haskell-ghc haskell-hlint html-tidy javascript-jshint javascript-gjslint json-jsonlint less lua perl php php-phpmd php-phpcs puppet-parser puppet-lint python-flake8 python-pylint rst ruby-rubocop ruby rust-cargo rust ruby-jruby sass scala scss sh-bash slim tex-chktex tex-lacheck xml-xmlstarlet xml-xmllint yaml-ruby)))
+   '(elm idris c/c++-clang c/c++-cppcheck coffee coffee-coffeelint css-csslint d-dmd elixir emacs-lisp emacs-lisp-checkdoc erlang go-gofmt go-build go-test haml haskell-ghc haskell-hlint html-tidy javascript-jshint javascript-gjslint json-jsonlint less lua perl php php-phpmd php-phpcs puppet-parser puppet-lint python-flake8 python-pylint rst ruby-rubocop ruby rust-cargo rust ruby-jruby sass scala scss sh-bash slim tex-chktex tex-lacheck xml-xmlstarlet xml-xmllint yaml-ruby))
  '(font-use-system-font t)
  '(foreground-color nil)
- '(frame-background-mode (quote dark))
+ '(frame-background-mode 'dark)
  '(indent-tabs-mode nil)
  '(inhibit-startup-screen t)
  '(js-indent-level 2)
  '(menu-bar-mode nil)
- '(ns-right-alternate-modifier (quote none))
- '(org-export-backends (quote (ascii beamer html icalendar latex md)))
- '(org-log-done (quote time))
+ '(ns-right-alternate-modifier 'none)
+ '(org-export-backends '(ascii beamer html icalendar latex md))
+ '(org-log-done 'time)
  '(org-todo-keyword-faces
-   (quote
-    (("IN PROGRESS" . "#cb4b16")
-     ("TO MERGE/DEPLOY" . "#b58900"))))
+   '(("IN PROGRESS" . "#cb4b16")
+     ("TO MERGE/DEPLOY" . "#b58900")))
  '(org-todo-keywords
-   (quote
-    ((sequence "TODO" "IN PROGRESS" "TO MERGE/DEPLOY" "|" "DONE"))))
+   '((sequence "TODO" "IN PROGRESS" "TO MERGE/DEPLOY" "|" "DONE")))
  '(package-selected-packages
-   (quote
-    (lsp-ui lsp-mode company-lsp typescript-mode magit ag alchemist cargo racer flycheck-rust rust-mode rainbow-identifiers zerodark-theme material-theme solaire-mode doom-themes yaml-mode terraform-mode counsel counsel-projectile flycheck-elm elm-mode cql-mode idris-mode groovy-mode scala-mode guide-key company markdown-mode swiper nyan-mode ace-window use-package solarized-theme rainbow-delimiters projectile multiple-cursors flycheck fill-column-indicator color-theme-solarized avy)))
+   '(yaml-mode sbt-mode scala-mode racer rainbow-delimiters lsp-ui lsp-mode company-lsp typescript-mode magit ag alchemist cargo flycheck-rust rainbow-identifiers material-theme doom-themes terraform-mode counsel counsel-projectile flycheck-elm elm-mode cql-mode idris-mode groovy-mode guide-key company markdown-mode nyan-mode ace-window multiple-cursors flycheck fill-column-indicator color-theme-solarized avy))
  '(projectile-globally-ignored-directories
-   (quote
-    (".idea" ".eunit" ".git" ".hg" ".fslckout" ".bzr" "_darcs" ".tox" ".svn" ".ensime_cache")))
+   '(".idea" ".eunit" ".git" ".hg" ".fslckout" ".bzr" "_darcs" ".tox" ".svn" ".ensime_cache"))
  '(python-indent-offset 4)
+ '(rust-indent-offset 2)
  '(safe-local-variable-values
-   (quote
-    ((intero-targets "intero-demo:lib" "intero-demo:test:intero-demo-test"))))
+   '((intero-targets "intero-demo:lib" "intero-demo:test:intero-demo-test")))
  '(scroll-bar-mode nil)
  '(show-trailing-whitespace t)
  '(tab-width 2)
  '(term-bind-key-alist
-   (quote
-    (("C-c C-c" . term-interrupt-subjob)
+   '(("C-c C-c" . term-interrupt-subjob)
      ("C-c C-e" . term-send-esc)
      ("C-p" . previous-line)
      ("C-n" . next-line)
@@ -87,13 +75,12 @@
      ("<C-backspace>" . term-send-backward-kill-word)
      ("M-r" . term-send-reverse-search-history)
      ("M-," . term-send-raw)
-     ("M-." . completion-at-point))))
+     ("M-." . completion-at-point)))
  '(term-unbind-key-list
-   (quote
-    ("C-z" "C-x" "C-c" "C-h" "C-y" "<ESC>" "<f10>" "<f5>" "<f1>" "<f10>")))
+   '("C-z" "C-x" "C-c" "C-h" "C-y" "<ESC>" "<f10>" "<f5>" "<f1>" "<f10>"))
  '(tool-bar-mode nil)
  '(tramp-default-method "ssh")
- '(uniquify-buffer-name-style (quote forward) nil (uniquify)))
+ '(uniquify-buffer-name-style 'forward nil (uniquify)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -113,7 +100,8 @@
 (use-package zerodark-theme
   :ensure t
   :config
-  (zerodark-setup-modeline-format))
+  (zerodark-setup-modeline-format)
+  (load-theme 'zerodark))
 
 (use-package rainbow-delimiters
   :ensure t
@@ -258,6 +246,7 @@
   :ensure t
   ;; Optional - enable lsp-mode automatically in scala files
   :hook (scala-mode . lsp)
+  :hook (elm-mode . lsp)
   :config (setq lsp-prefer-flymake nil))
 
 (use-package lsp-ui
@@ -266,11 +255,6 @@
 ;; Add company-lsp backend for metals
 (use-package company-lsp
   :ensure t)
-
-(use-package idris-mode
-  :ensure t
-  :config
-  (setq idris-interpreter-path "idris"))
 
 (use-package elm-mode
   :ensure t
@@ -296,7 +280,6 @@
 (setq rm-blacklist (mapconcat 'identity [" hl-p" " Guide" " Projectile" "ivy" "company"] "\\|"))
 
 (add-hook 'before-save-hook 'whitespace-cleanup)
-(add-hook 'before-save-hook 'lsp-format-buffer)
 
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
